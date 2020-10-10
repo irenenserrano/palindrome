2f6f02c17592319f907c2361632a6e6376bbf14f
Irene Serrano <iserrano@mills.edu>
2020-10-10 16:22:41 -0700

    Correct javadoc

--- src/edu/mills/cs180/PalindromeChecker.java
+++ src/edu/mills/cs180/PalindromeChecker.java
@@ -3,7 +3,7 @@ package edu.mills.cs180;
 /**
  * A static utility class for testing whether strings are palindromes.
  *
- * @author Ellen Spertus
+ * @author Irene Serrano
  */
 public class PalindromeChecker {
     private PalindromeChecker() {}
